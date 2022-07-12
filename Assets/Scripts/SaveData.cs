b7aea9c7a8bcb75a898790ce526226000c5f27cb
AjinRenfire <ajinra4@outlook.com>
2022-07-12 17:12:39 +0530

    added highscore

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SaveData {
+
+    public string Name; 
+    public int HighScore ;
+}
